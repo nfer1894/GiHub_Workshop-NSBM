6296136f2f5030e01f2ccb9a5eeef69cb9eefd07
Sathsilu-Gunawardana <ggysgunawardana@gmail.com>
2026-03-17 12:02:03 +0530

    Changed the welcome massage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from task import add_task, list_tasks, complete_task, remove_task
 
-WELCOME_MESSAGE = "Welcome to Task Manager CLI! Nikilaaaaa @ nsbm "
+WELCOME_MESSAGE = "Welcome to Task Manager CLI! Sathsilu"
 
 
 def show_help():
