9426e2c2ac9a02ed81736b94045e38d16a6e6c64
SanukaWaravita <sanukawaravita@gmail.com>
2026-03-17 11:42:33 +0530

    feat: personalise welcome message

--- task.py
+++ task.py
@@ -11,7 +11,7 @@ def add_task(title):
     }
     tasks.append(task)
     save_tasks(tasks)
-    print(f"Task added: '{title}'")
+    print(f"Yay! Task has been added: (changed by Sanuka) '{title}'")
 
 
 def list_tasks():
